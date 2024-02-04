1c19089299007252b774d3c52695fe6adc761ea2
sanujasethi06 <sethisanuja1998@gmail.com>
2024-02-04 12:48:14 +0530

    Swiper slider

--- src/components/GameSwiper.jsx
+++ src/components/GameSwiper.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/navigation'
 import './gameSwiper.css'
 
-const GameSwiper = () => {
+import { EffectCoverflow,Navigation,Autoplay} from 'swiper/modules'
+
+const GameSwiper = ({ games }) => {
+    const [active, setActive] = useState(false);
+    const handToggleVideo = () => {
+        setActive(!active);
+    }
   return (
-    <div>
+      <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          navigation={true}
+          loop={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+              rotate: 35,
+              stretch: 200,
+              depth: 250,
+              modifier: 1,
+              slideShadows:true,
+          }}
+        //   autoplay={{
+        //       delay: 2500,
+        //       disableOnInteraction:false
+        //   }}
+          modules={[EffectCoverflow, Navigation, Autoplay]}
+      className='gameSwiper'>
+          {games.map(game => (
+              <SwiperSlide key={game._id}>
+                  <div className="gameSlider">
+                      <img src={game.img} alt="Game Image" className="" />
+                      <div className="content">
+                          <h2 className="">
+                              {game.title}
+                          </h2>
+                          <p>
+                              {game.description}
+                          </p>
+                          <div className="buttons">
+                              <a href="#" className="orderBtn">Order Now</a>
+                              <a href="#" className={`playBtn ${active? 'active':undefined}`} onClick={handToggleVideo}>
+                                  <span className="pause">
+                                      <i className="bi bi-pause-fill"></i>
+                                  </span>
+                                  <span className="play">
+                                      <i className="bi bi-play-fill"></i>
+                                  </span>
+                              </a>
+                          </div>
+                      </div>
+                  </div>
+                  <h4>{game.title}</h4>
+                  
+              </SwiperSlide>
+          ))
+              
+          }
       
-    </div>
+    </Swiper>
   )
 }
 
