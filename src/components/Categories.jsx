7edd06082c78bd2c5e14e2c09993df7598c2e81b
sanujasethi06 <sethisanuja1998@gmail.com>
2024-02-07 13:12:18 +0530

    Completed categories page

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,25 +1,38 @@
 import React, { useState } from 'react'
 import './categories.css'
 import filterListData from '../data/filterListData'
+import GameCard from './GameCard';
 
 const Categories = ({ games, reference }) => {
-
+    const [data, setData] = useState(games);
     const [filters, setFilters] = useState(filterListData)
     const handleFilterGames = (category) => {
-        const newFilters=filters.map(filter => {
+        const newFilters = filters.map(filter => {
             filter.active = false;
             if (filter.name === category) {
                 filter.active = true;
             }
             return filter;
         });
+     
         setFilters(newFilters);
+        if (category === 'All') {
+            setData(games);
+            return;
+        }
+        setData(games.filter(game => game.category === category));
+        
+    };
+    const [text, setText] = useState('');
+    const handleSearchGames = e => { 
+        setData(games.filter(game => game.title.toLowerCase().includes(e.target.value.toLowerCase())));
+        setText(e.target.value);
     }
   return (
       <section className='categories' id='categories' ref={reference}>
           <div className="container-fluid mt-2">
               <div className="row">
-                  <div className="col-lg-8">
+                  <div className="col-lg-8 flex align-items-center justify-content-start">
                       <ul className="filters">
                           {
                               filters.map(filter => (
@@ -30,9 +43,20 @@ const Categories = ({ games, reference }) => {
                           
                       </ul>
                   </div>
-                  <div className="col-lg-4">
+                  <div className="col-lg-4 flex align-items-center justify-content-end">
+                      <div className="search">
+                          
+                      <i className="bi bi-search"></i>
+                          <input type="text" name='Search' placeholder='Search'
+                              value={text } onChange={handleSearchGames} />
                       
+                      </div>
                   </div>
+              </div>
+              <div className="row">
+                  {data.map(game => (
+                      <GameCard key={game._id} game={game}/>
+                  ))}
               </div>
           </div>
     </section>
